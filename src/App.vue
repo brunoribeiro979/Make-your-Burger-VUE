15e02f95facb3e71369f90c335f681d440b73c56
Bruno Ribeiro <bruno.santos979@yahoo.com.br>
2022-10-03 17:23:07 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <RouterView />
+  <Footer />
+</template>
+
+<script >
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    };
+  },
+};
+</script>
+
+
+
+<style>
+* {
+  font-family: Helvetica;
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
